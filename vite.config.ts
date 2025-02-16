a79368b42ac1ecf49ca209b585879b46cd03bd3f
Dani <danigd71@gmail.com>
2025-02-16 13:41:29 +0100

    Configuración para despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="node" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 3000, // Usa el puerto de Render o el 3000 por defecto
+    host: true, // Hace la app accesible desde fuera
+  }
 })
